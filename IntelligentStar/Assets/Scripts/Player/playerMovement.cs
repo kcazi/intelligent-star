11fa5c37e475f28a53d56ad01fa75aaba2b0b595
Spiderflame <carsonsmith4ever@gmail.com>
2026-09-11 18:31:16 -0400

    Basic Movement
    +Added the ability to move left and right with A and D.
    +Displays debug message when pressing Space for Jump
    
    -Need to add gravity, platforms, viewing direction/ slow down when moving in opposite directions, and jumping

--- IntelligentStar/Assets/Scripts/Player/playerMovement.cs
+++ IntelligentStar/Assets/Scripts/Player/playerMovement.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
+    
+    public float moveSpeed;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float direction = 0f;
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            direction = -1f;
+        }
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            direction = 1f;
+        }
+
+        if (Keyboard.current.spaceKey.isPressed)
+        {
+            Debug.Log("Space Pressed");
+        }
+
+        transform.position += Vector3.right * direction * moveSpeed * Time.deltaTime;
     }
 }
