562d7d300b953a0b47aae021356550f1947120d3
Jake Halbach <jhalbach@charlotte.edu>
2026-09-22 17:34:23 -0400

    added platform, gravity (WIP), dynamic jumping (WIP)
    we can leave the gravity and jump as is now (with maybe some modifications) you guys mess around with it and see what feels good.

--- IntelligentStar/Assets/Scripts/Player/playerMovement.cs
+++ IntelligentStar/Assets/Scripts/Player/playerMovement.cs
@@ -3,35 +3,90 @@ using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
-    
-    public float moveSpeed;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+
+    public float jumpForce = 12f;
+    public float gravity = 30f;
+    public float fallGravity = 40f;
+
+    // can change if jump feels awkward
+    public float jumpCutMultiplier = 0.1f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
     void Start()
     {
-
+        // find obj rb
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // player isnt pressing a move key
         float direction = 0f;
 
         if (Keyboard.current.aKey.isPressed)
         {
+            // multiplies movement vector and goes left
             direction = -1f;
         }
 
         if (Keyboard.current.dKey.isPressed)
         {
+            // multiplies movement vector and goes right
             direction = 1f;
         }
 
-        if (Keyboard.current.spaceKey.isPressed)
+        // Start jump
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
         {
-            Debug.Log("Space Pressed");
+            // applies jumpforce value to obj y value
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
 
-        transform.position += Vector3.right * direction * moveSpeed * Time.deltaTime;
+        // releasing Space early cuts the upward velocity
+        if (Keyboard.current.spaceKey.wasReleasedThisFrame && rb.linearVelocity.y > 0f)
+        {
+            // if space is released multiply upward velocity by cutmultiplier
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
+        }
+
+        // Horizontal movement
+        rb.linearVelocity = new Vector2(direction * moveSpeed, rb.linearVelocity.y);
+    }
+
+    // gravity for player/creatures/objs
+    void FixedUpdate()
+    {
+        float currentGravity = gravity;
+
+        if (rb.linearVelocity.y < 0f)
+        {
+            // lowkey dont know if this is necessary I was just following a video that said this was a good idea
+            currentGravity = fallGravity;
+        }
+
+        rb.linearVelocity += Vector2.down * currentGravity * Time.fixedDeltaTime;
+    }
+
+    // collision with ground, checks if ground is actually whats colliding (prevents jumping off wall collision)
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            // Check that the surface is underneath the player
+            if (contact.normal.y > 0.5f)
+            {
+                isGrounded = true;
+                return;
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        // sets false after stops touching ground
+        isGrounded = false;
     }
 }
